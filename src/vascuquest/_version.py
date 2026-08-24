9690ad31814325845419a7ea197156168be3255b
Khalid M. Saqr <73785444+khalid-saqr@users.noreply.github.com>
2026-08-24 09:16:32 +0300

    Add VascuQuest package version module

---
+++ src/vascuquest/_version.py
@@ -0,0 +1,7 @@
+"""VascuQuest package version.
+
+This module is intentionally dependency-free and side-effect free so package
+version information can be imported safely during normal runtime and tooling.
+"""
+
+__version__ = "0.1.0.dev0"
