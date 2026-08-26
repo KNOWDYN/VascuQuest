55246799fa40076e1662cfaf4912abf9940353ef
Khalid M. Saqr <73785444+khalid-saqr@users.noreply.github.com>
2026-08-26 15:14:15 +0300

    Align runtime version with v0.1.0

--- src/vascuquest/_version.py
+++ src/vascuquest/_version.py
@@ -4,4 +4,4 @@ This module is intentionally dependency-free and side-effect free so package
 version information can be imported safely during normal runtime and tooling.
 """
 
-__version__ = "0.1.0.dev0"
+__version__ = "0.1.0"
